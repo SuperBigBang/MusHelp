b5da3ec0ad2eb589135f81bb8a9430e3969fce92
SuperBigBang <superbigbang@yandex.ru>
2018-12-16 17:00:44 +0900

    fix: Buy popup Window layout fix feat: prepare for Realm database realise

--- app/src/main/java/com/superbigbang/mushelp/di/BaseComponent.java
+++ app/src/main/java/com/superbigbang/mushelp/di/BaseComponent.java
@@ -3,18 +3,13 @@ package com.superbigbang.mushelp.di;
 import android.content.Context;
 
 import com.superbigbang.mushelp.di.modules.ContextModule;
-import com.superbigbang.mushelp.di.modules.StarbuzzDatabaseHelperModule;
 
 import javax.inject.Singleton;
 
 import dagger.Component;
 
 @Singleton
-@Component(modules = {ContextModule.class, StarbuzzDatabaseHelperModule.class})
+@Component(modules = {ContextModule.class})
 public interface BaseComponent {
     Context getContext();
-
-    StarbuzzDatabaseHelperModule getStarbuzzDatabaseHelperModule();
-
-    // void inject(DrinkActivityPresenter drinkActivityPresenter);
 }
